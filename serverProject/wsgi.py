abf42929d7f3b3c2e05558b9e94cd87916c07402
Kuchnast <62248017+kuchnast@users.noreply.github.com>
2022-05-16 13:47:48 +0200

    Page template

--- serverProject/wsgi.py
+++ serverProject/wsgi.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
-import serial
+import time
+import serial_port
 
 # port = "/dev/ttyACM1"
 # ser=serial.Serial(port,115200)
@@ -17,12 +18,33 @@ app = Flask(__name__)
 @app.route("/", methods=["POST", "GET"])
 def glowna():
     if request.method == "POST":
-        # return render_template('index.html')
-        zmienna = request.form["wrt"]
-        # return "Wczytalem "+zmienna
-        return render_template('index.html')
+        zmienna = request.form["wartosc"]
+        if len(zmienna)>0:
+            if zmienna[0] =='t':
+                return render_template('index.html',printTime=1,time=100)
+            elif zmienna[0] =='d':
+                try:
+                    convDate=int(zmienna[1:len(zmienna)])
+                    if convDate>2147483647:
+                        return render_template('index.html',error=1,errorMess="Max value is 2147483647")
+                    date2=time.ctime(convDate)
+                    return render_template("index.html",a=1,date=date2)
+                except:
+                    return render_template('index.html',error=1,errorMess="Date command - d<value>")
+            else:
+                return render_template('index.html',error=1,errorMess="Wrong command")
+        else:
+            return render_template('index.html',error=1,errorMess="Write a command")
     else:
         return render_template('index.html')
+####
+#
+#
+
+@app.route("/revdata")
+def recivedData():
+
+  return render_template('recivedData.html')
 # return "<p>Hello, World!</p>"
 # return "<p>Hello, World!</p>"
 
